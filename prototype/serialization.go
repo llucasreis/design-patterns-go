bda66b24e75353df47b0682f8f689850cd3ce43c
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-07-24 18:30:24 -0400

    Prototype Pattern (#3)
    * feat: add simple prototype
    
    * feat: add prototype pattern with serialization
    
    * feat: add prototype with factory
    
    * feat: update main to use prototype functions

---
+++ prototype/serialization.go
@@ -0,0 +1,45 @@
+package prototype
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
+type Address2 struct {
+	StreetAddress2, City, Country string
+}
+
+type Person2 struct {
+	Name     string
+	Address2 *Address2
+	Friends  []string
+}
+
+func (p *Person2) DeepCopy() *Person2 {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	_ = e.Encode(p)
+
+	d := gob.NewDecoder(&b)
+	result := Person2{}
+	_ = d.Decode(&result)
+	return &result
+}
+
+func RunSerialization() {
+	john := Person2{Name: "John",
+		Address2: &Address2{
+			StreetAddress2: "123 London Road",
+			City:           "London",
+			Country:        "UK",
+		},
+		Friends: []string{"Chris", "Matt"}}
+
+	jane := john.DeepCopy()
+	jane.Name = "Jane"
+	jane.Address2.StreetAddress2 = "321 Baker Street"
+
+	fmt.Printf("%+v, %+v\n", john, john.Address2)
+	fmt.Printf("%+v, %+v\n", jane, jane.Address2)
+}
