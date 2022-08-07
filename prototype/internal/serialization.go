8e5a805950bb742dde96a302196b62a9bd3e51de
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-08-07 11:08:39 -0400

    Refactor: Use main for each director (#5)
    * refactor: change builder to have its own main
    
    * refactor: change factory to have its own main
    
    * refactor: change prototype to have its own main
    
    * refactor: delete main from root directory

--- prototype/internal/serialization.go
+++ prototype/internal/serialization.go
@@ -1,4 +1,4 @@
-package prototype
+package internal
 
 import (
 	"bytes"
---
+++ prototype/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/llucasreis/design-patterns-go/prototype/internal"
+
+func main() {
+	internal.RunCopy()
+	internal.RunSerialization()
+	internal.RunFactory()
+}
