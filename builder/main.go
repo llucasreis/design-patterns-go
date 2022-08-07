2d1f91ee97e1ddb9dd1e22a44129af28ac9bb390
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-08-07 12:07:01 -0300

    refactor: change builder to have its own main

---
+++ builder/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/llucasreis/design-patterns-go/builder/internal"
+
+func main() {
+	internal.RunBuilderSimple()
+	internal.RunBuilderMultiple()
+	internal.RunBuilderParameter()
+	internal.RunBuilderFunctional()
+}
