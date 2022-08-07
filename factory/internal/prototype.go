1c126318d1d215afeb38b63ee59baf50d37fc9f3
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-08-07 12:07:13 -0300

    refactor: change factory to have its own main

--- factory/internal/prototype.go
+++ factory/internal/prototype.go
@@ -1,4 +1,4 @@
-package factory
+package internal
 
 import "fmt"
 
---
+++ factory/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/llucasreis/design-patterns-go/factory/internal"
+
+func main() {
+	internal.RunFunction()
+	internal.RunInterface()
+	internal.RunGenerator()
+}
