c62e4722d2312762a5d2f10b315082a23ca716d8
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-11-06 11:16:05 -0300

    Iterator Pattern (#15)
    * feat: add differents iterator pattern impl
    
    * feat: add binary tree example for iterator pattern
    
    * feat: add main file for iterator

---
+++ iterator/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/llucasreis/design-patterns-go/iterator/internal"
+)
+
+func main() {
+	internal.RunIteration()
+	fmt.Println("------")
+	internal.RunTreeTraversal()
+}
