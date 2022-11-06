8f20c0c2dfb9b8c7f018ea8702cdcb9dfb8336c8
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2022-11-06 11:48:13 -0300

    feat: add main file for flyweight

---
+++ flyweight/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/llucasreis/design-patterns-go/flyweight/internal"
+)
+
+func main() {
+	internal.RunTextFormat()
+	fmt.Println("------------")
+	internal.RunUserNames()
+}
